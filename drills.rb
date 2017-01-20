d5df83777dab649616bc1ab95c38e5112c71c7c6
aapiane09 <aapiane09@gmail.com>
2017-01-19 18:51:51 -0800

    Did homework exercises

--- drills.rb
+++ drills.rb
@@ -2,21 +2,32 @@
 #### USING ARGUMENTS ####
 #########################
 #say_hello
-  # returns 'hello'
+def say_hello
+  "hello"
+end
+say_hello
 
 #echo
-  # echoes (returns) the input string
+def echo (word)
+  word
+end
+echo("Hello")
 
 #get_fruit
-  # returns the fruit that corresponds to the given id
-  # does not raise an error when the id argument is missing
-  # defaults to 'apple' when no id argument is supplied
+def get_fruit (id = 0)
+  ["apple", "pear"][id]
+end
 
 #how_many_args
+def how_many_args ()
+  #.splat
+end
   # accepts any number of arguments without error
   # returns the total number of arguments passed to the function
 
 #find_answer
+def find_answer
+end
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # raises an error when given non-keyword arguments
@@ -25,32 +36,61 @@
 #### MANIPULATING STRINGS ####
 ##############################
 #first_char
+def first_char(word)
+  word[0].downcase
+end
   # returns the first letter of the given word
   # lowercases the first letter of the given word
 
 #polly_wanna
+def polly_wanna (word)
+  word*3
+end
   # includes the given word in its return value
   # repeats the given word 3 times
 
 #count_chars
+def count_chars (string)
+  string.length
+end
   # returns the number of characters in the given string
 
 #yell
+def yell (string)
+  string.upcase + "!"
+end
   # convert the given message to uppercase
   # adds an exclamation point to the end of the given message
 
 #to_telegram
+def to_telegram (word)
+  word.gsub(/\./, ' STOP')
+end
+to_telegram("Hello. It's me. I've been wondering if after all these years we'd finally meet." )
   # replaces periods in the input string with ' STOP'
 
 #spell_out
+def spell_out (string)
+  string.downcase.split(//).join("-")
+end
   # returns the input string, with characters seperated by dashes
   # converts the input string to lowercase
 
 #seperate
+def separate (string, joiner = "-")
+  string.split(//).join(joiner)
+end
   # seperates characters in the input string with a custom seperator, when supplied with one
   # seperates characters in the input string with dashes (by default)
 
 #croon
+def croon (string)
+  second = []
+  array = string.split.each { |i|
+    second.push(i.split(//).join("-"))
+  }
+  second.join(" ")
+end
   # seperates characters in each word of the input phrase with dashes
   # preserves whitespace between words
 
@@ -79,15 +119,52 @@
 #### LOOPS & ITERATORS ####
 ###########################
 #count_spaces
+# def count_spaces (string)
+#   letters = string.split(//)
+#   counts = Hash.new(0)
+#   letters.each do
+#     if " "
+#       counts[letter] += 1
+#     end
+#   end
+#   counts.length
+# end
+
+def count_spaces(str)
+  count = 0
+  space = " "
+  str.each_char do |char|
+    if char == space
+      count += 1
+    end
+  end
+  count
+end
   # counts the spaces in a given string
 
 #string_lengths
+# def string_lengths (array)
+#   new_array = []
+#   array.each do |string|
+#     length = string.length
+#     new_array.push(length)
+#   end
+# end
+def string_lengths(list)
+  list.map {|str| str.length }
+end
   # converts a given list of strings to a list of string lengths
 
 #remove_falsy_values
   # filters out falsy values from the input array
 
 #exclude_last
+# def exclude_last (input)
+#   input.splice[input.length - 1]
+# end
+def exclude_last(sequence)
+  sequence[0..-2]
+end
   # removes the last item from an input array
   # removes the last item from an input string
 
@@ -116,6 +193,14 @@
 #### MANIPULATING NUMBERS ####
 ##############################
 #count_to
+def count_to (n)
+  array = []
+  count = 0
+  while count <= n
+    count += 1
+    array.push(count.round)
+  end
+end
   # returns an array containing every integer from 0 to the input n
   # rounds off decimals
 
